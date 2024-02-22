9c8cb127c2fa4045f279f96dd981027fe5ac90a2
Vaishali Jain <vaishalijain.vj.10@gmail.com>
2024-02-22 17:09:53 +0530

    complete the OAuth functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -51,7 +51,7 @@ export default function SignUp() {
       formDataCopy.timestamp = serverTimestamp();
 
       await setDoc(doc(db, "users", user.uid), formDataCopy);
-      toast.success("Sign up was successful")
+      // toast.success("Sign up was successful")
       navigate("/");
     } catch (error) {
       toast.error("Something went wrong with the  registration");
