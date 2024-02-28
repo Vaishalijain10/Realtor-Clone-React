ac1ec58cba289caf4e0ef5da26b61593d87d8381
Vaishali Jain <vaishalijain.vj.10@gmail.com>
2024-02-28 14:32:56 +0530

    Add functionality of onchange and onsubmit functions to the create listing page

--- src/App.js
+++ src/App.js
@@ -6,32 +6,32 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
-import {  ToastContainer,toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
-
 
 function App() {
   return (
     <>
       <Router>
-
         <Header />
-
         <Routes>
-          <Route path="/" element={<Home />}> </Route>
-          <Route path="/Profile" element={<PrivateRoute />}>  
-          <Route path="/Profile" element={<Profile />}> </Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Profile" element={<PrivateRoute />}>
+            <Route path="/Profile" element={<Profile />}></Route>
           </Route>
-        
-          <Route path="/SignIn" element={<SignIn />}> </Route>
-          <Route path="/SignUp" element={<SignUp />}> </Route>
-          <Route path="/ForgotPassword" element={<ForgotPassword />}> </Route>
-          <Route path="/Offers" element={<Offers />}> </Route>
-          <Route path="/CreateListing" element={< CreateListing/>}> </Route>
+          <Route path="/SignIn" element={<SignIn />}></Route>
+          <Route path="/SignUp" element={<SignUp />}></Route>
+          <Route path="/ForgotPassword" element={<ForgotPassword />}></Route>
+          <Route path="/Offers" element={<Offers />}></Route>
+          {/* PrivateRoute is to give security/ authentication to the URL */}
+          <Route path="/CreateListing" element={<PrivateRoute />}>
+            <Route path="/CreateListing" element={<CreateListing />}></Route>
+          </Route>
         </Routes>
       </Router>
+
       <ToastContainer
         position="bottom-center"
         autoClose={5000}
@@ -43,8 +43,7 @@ function App() {
         draggable
         pauseOnHover
         theme="dark"
-    
-        />
+      />
     </>
   );
 }
